ffaddafe1fb395a3a99fb33ddde20c0d13066e5f
Avi Thour <thouravi@gmail.com>
2018-08-05 13:33:17 +0530

    Python Based Dicegame
    User inputs the amount to bet and multiplier on the base amount.
    
    A random number is generated between 1 to 100 which decided the win/lose.

---
+++ dicegame.py
@@ -0,0 +1,31 @@
+import random
+result = random.randint(1,100)
+
+print ("Set the amount to bet:")
+amount = int(input())
+
+print ("Okay, now select the multiplier of winning.")
+chance = int(input())
+
+if chance == 1:
+    win = 100
+elif chance == 2:
+    win = 50
+elif chance == 3:
+    win = 33
+elif chance == 4:
+    win = 12
+else:
+    print ("Maximum multiplier value is 4!")
+    exit()
+
+prize = amount * chance
+
+print ("Your base amount is $" + str(amount) + " and If you win, you will get $" + str(prize) + ".")
+print ("You will only win if your number is less than or equals to " + str(win) + ".")
+
+
+if result <= win:
+    print ("You won $" + str(prize) + "! The number is " + str(result))
+else:
+    print ("You lost $" + str(prize) + "! The number is " + str(result))
